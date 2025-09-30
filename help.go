e75f4a22698a83d29746df9dc8cef6eaf360e273
Tom Cole <tomcole@mac.com>
2025-09-30 15:20:55 -0400

    fix: cleanup help text slightly

--- help.go
+++ help.go
@@ -17,8 +17,8 @@ options:
   -d, --dictionary <file>   Add this dictionary file to the test dictionary
   -f, --filter <string>     Only run tests that contain the given string in their names
   -h, --help                Show this help message and exit
-  -r, --rest                Enable REST mode, which makes the server return a JSON response
-  -v, --verbose             Enable Verbose output
+  -r, --rest                Enable REST logging, which displays the text of each JSON response
+  -v, --verbose             Enable verbose logging output
   -x, --define <key=value>  Define a value for a variable in the test dictionary (can be repeated)
   
   See the project README.md file for information on the format of test files that are located
